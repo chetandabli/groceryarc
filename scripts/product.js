21f60fd2e0d10864f219494cbed3fbf4046f4828
Vipin4147 <vipin4147@gmail.com>
2022-12-18 03:02:04 +0530

    pagination

--- scripts/product.js
+++ scripts/product.js
@@ -29,18 +29,21 @@ let d10_15=document.getElementById("d10_15");
 let d15_25=document.getElementById("d15_25");
 let d25=document.getElementById("d25");
 
-let get_data=async()=>{
+let page = document.getElementById("page");
 
+let get_data=async()=>{
+ let base_url=`${url}`;
     let res=await fetch(`${url}`)
     let data= await res.json();
     console.log(data)
-    append_data(data)
+    let len =data.length;
+    append_data(data,base_url,len)
     f_v.innerText=`(${data.length})`
 }
 
 get_data();
 
-const append_data=(data)=>{
+const append_data=(data,base_url,len)=>{
 
 cont.innerHTML=null;
 f_v.innerText=`(${data.length})`;
@@ -113,28 +116,42 @@ data.forEach((el)=>{
    
    
 })
+ let n=Math.ceil(data.length/5);
+ page.innerHTML=null;
+ for(let i=0;i<n;i++)
+ {
+    let pbtn=document.createElement("button");
+    pbtn.setAttribute("class","pbtn");
+    pbtn.innerText=i+1;
+    page.append(pbtn);
+    let p=i+1;
+    pbtn.onclick=()=>{
+        pagination(p,base_url,n)
+    }
+   
+ }
  
-
 }
 
 let fruit =document.getElementById("fruit");
 fruit.onclick=()=>{
     get_data();
     
-    heading.innerText="Fruits & Vegetables (36)";
    
+    heading.innerText=`Fruits & Vegetables (${36})`;
 }
 
 let sprouts =document.getElementById("sprouts");
 
 sprouts.onclick=async ()=>{
+    let base_url=`${main_url}/grainsMasala`;
  let res =await fetch (`${main_url}/grainsMasala`);
 
  let data = await res.json();
 
  console.log(data);
 
- append_data(data);
+ append_data(data,base_url);
 
  heading.innerText=`Cuts & Sprouts (${data.length})`
  
@@ -144,13 +161,15 @@ sprouts.onclick=async ()=>{
 let veggies =document.getElementById("veggies");
 
 veggies.onclick=async ()=>{
+
+    let base_url=`${main_url}/bakeryCake`;
  let res =await fetch (`${main_url}/bakeryCake`);
 
  let data = await res.json();
 
  console.log(data);
 
- append_data(data);
+ append_data(data,base_url);
 
  heading.innerText=`Exotic Fruits & Veggies (${data.length})`
 }
@@ -158,13 +177,16 @@ veggies.onclick=async ()=>{
 let bunches =document.getElementById("bunches");
 
 bunches.onclick=async ()=>{
+
+  let base_url=  `${main_url}/beverages`;
+
  let res =await fetch (`${main_url}/beverages`);
 
  let data = await res.json();
 
  console.log(data);
 
- append_data(data);
+ append_data(data,base_url);
 
  heading.innerText=`Flower Bouquets, Bunches (${data.length})`
 }
@@ -172,13 +194,15 @@ bunches.onclick=async ()=>{
 let ff =document.getElementById("ff");
 
 ff.onclick=async ()=>{
+
+    let base_url=`${main_url}/cleaningHouse`;
  let res =await fetch (`${main_url}/cleaningHouse`);
 
  let data = await res.json();
 
  console.log(data);
 
- append_data(data);
+ append_data(data,base_url);
 
  heading.innerText=`Fresh Fruits (${data.length})`
 
@@ -187,39 +211,46 @@ ff.onclick=async ()=>{
 let fv =document.getElementById("fv");
 
 fv.onclick=async ()=>{
+
+    let base_url=`${main_url}/kitchenGarden`;
+    
  let res =await fetch (`${main_url}/kitchenGarden`);
 
  let data = await res.json();
 
  console.log(data);
 
- append_data(data);
+ append_data(data,base_url);
  heading.innerText=`Fresh Vegetables (${data.length})`
 }
 
 let hs =document.getElementById("hs");
 
 hs.onclick=async ()=>{
+
+    let base_url=`${main_url}/eggMeat`;
  let res =await fetch (`${main_url}/eggMeat`);
 
  let data = await res.json();
 
  console.log(data);
 
- append_data(data);
+ append_data(data,base_url);
  heading.innerText=`Herbs & seasonings (${data.length})`
 }
 
 let ofv =document.getElementById("of&v");
 
 ofv.onclick=async ()=>{
+
+    let base_url=`${main_url}/gourmetFood`;
  let res =await fetch (`${main_url}/gourmetFood`);
 
  let data = await res.json();
 
  console.log(data);
 
- append_data(data);
+ append_data(data,base_url);
  heading.innerText=`Organic Fruits & Vegetables (${data.length})`
 }
 
@@ -233,13 +264,14 @@ ofv.onclick=async ()=>{
 bb_combo.onchange=async (event)=>{
     if(event.target.checked)
     {
+        let base_url=`${url}?proName_like=bb Combo`;
         let result =await fetch (`${url}?proName_like=bb Combo`);
 
         let fdata = await result.json();
        
         
        
-        append_data(fdata);
+        append_data(fdata,base_url);
         if(fdata.length==0)
        {
         cont.innerHTML=`<h1>No Item Found</h1>`;
@@ -255,13 +287,14 @@ bb_combo.onchange=async (event)=>{
 fresho.onchange=async (event)=>{
     if(event.target.checked)
     {
+        let base_url=`${url}?proName_like=Fresho`;
         let result =await fetch (`${url}?proName_like=Fresho`);
 
         let fdata = await result.json();
        
         
        
-        append_data(fdata);
+        append_data(fdata,base_url);
         if(fdata.length==0)
         {
          cont.innerHTML=`<h1>No Item Found</h1>`;
@@ -277,12 +310,13 @@ fresho.onchange=async (event)=>{
 bb_royal.onchange=async (event)=>{
     if(event.target.checked)
     {
+        let base_url=`${url}?proName_like=BB Royal`;
         let result =await fetch (`${url}?proName_like=BB Royal`);
 
         let fdata = await result.json();
        
        
-        append_data(fdata);
+        append_data(fdata,base_url);
         if(fdata.length==0)
         {
          cont.innerHTML=`<h1>No Item Found</h1>`;
@@ -298,13 +332,14 @@ bb_royal.onchange=async (event)=>{
 fortune.onchange=async (event)=>{
     if(event.target.checked)
     {
+        let base_url=`${url}?proName_like=Fortune`;
         let result =await fetch (`${url}?proName_like=Fortune`);
 
         let fdata = await result.json();
        
        
        
-        append_data(fdata);
+        append_data(fdata,base_url);
         if(fdata.length==0)
         {
          cont.innerHTML=`<h1>No Item Found</h1>`;
@@ -319,11 +354,12 @@ fortune.onchange=async (event)=>{
 sunpure.onchange=async (event)=>{
     if(event.target.checked)
     {
+        let base_url=`${url}?proName_like=Sunpure`;
         let result =await fetch (`${url}?proName_like=Sunpure`);
 
         let fdata = await result.json();
         
-        append_data(fdata);
+        append_data(fdata,base_url);
         if(fdata.length==0)
        {
         cont.innerHTML=`<h1>No Item Found</h1>`;
@@ -339,13 +375,14 @@ sunpure.onchange=async (event)=>{
 p20.onchange=async (event)=>{
     if(event.target.checked)
     {
+        let base_url=`${url}?price_lte=19`;
         let result =await fetch (`${url}?price_lte=19`);
 
         let fdata = await result.json();
        
         
        
-        append_data(fdata);
+        append_data(fdata,base_url);
         if(fdata.length==0)
        {
         cont.innerHTML=`<h1>No Item Found</h1>`;
@@ -361,13 +398,14 @@ p20.onchange=async (event)=>{
 p21_50.onchange=async (event)=>{
     if(event.target.checked)
     {
+        let base_url=`${url}?price_gte=21&price_lte=50`;
         let result =await fetch (`${url}?price_gte=21&price_lte=50`);
 
         let fdata = await result.json();
        
         
        
-        append_data(fdata);
+        append_data(fdata,base_url);
         if(fdata.length==0)
         {
          cont.innerHTML=`<h1>No Item Found</h1>`;
@@ -382,13 +420,14 @@ p21_50.onchange=async (event)=>{
 
 p51_100.onchange=async (event)=>{
     if(event.target.checked)
-    {
+    { 
+        let base_url=`${url}?price_gte=51&price_lte=100`;
         let result =await fetch (`${url}?price_gte=51&price_lte=100`);
 
         let fdata = await result.json();
        
        
-        append_data(fdata);
+        append_data(fdata,base_url);
         if(fdata.length==0)
         {
          cont.innerHTML=`<h1>No Item Found</h1>`;
@@ -404,13 +443,14 @@ p51_100.onchange=async (event)=>{
 p101_200.onchange=async (event)=>{
     if(event.target.checked)
     {
+        let base_url=`${url}?price_gte=101&price_lte=200`;
         let result =await fetch (`${url}?price_gte=101&price_lte=200`);
 
         let fdata = await result.json();
        
        
        
-        append_data(fdata);
+        append_data(fdata,base_url);
         if(fdata.length==0)
         {
          cont.innerHTML=`<h1>No Item Found</h1>`;
@@ -425,11 +465,12 @@ p101_200.onchange=async (event)=>{
 p201_500.onchange=async (event)=>{
     if(event.target.checked)
     {
+        let base_url=`${url}?price_gte=201&price_lte=500`;
         let result =await fetch (`${url}?price_gte=201&price_lte=500`);
 
         let fdata = await result.json();
         
-        append_data(fdata);
+        append_data(fdata,base_url);
         if(fdata.length==0)
        {
         cont.innerHTML=`<h1>No Item Found</h1>`;
@@ -445,11 +486,12 @@ p201_500.onchange=async (event)=>{
 p501.onchange=async (event)=>{
     if(event.target.checked)
     {
+        let base_url= `${url}?price_gte=501`;
         let result =await fetch (`${url}?price_gte=501`);
 
         let fdata = await result.json();
         
-        append_data(fdata);
+        append_data(fdata,base_url);
         if(fdata.length==0)
        {
         cont.innerHTML=`<h1>No Item Found</h1>`;
@@ -474,30 +516,33 @@ p501.onchange=async (event)=>{
 let sorty = document.getElementById("sorty");
 
 sorty.onchange=async()=>{
-   
+   let base_url;
  if(sorty.value=="Price-Low to High")
  {
+     base_url=`${url}?_sort=price&_order=asc`;
    let res =await fetch(`${url}?_sort=price&_order=asc`)
 
    let data =await res.json();
 
-   append_data(data);
+   append_data(data,base_url);
  }
  else if(sorty.value=="Price-High to Low")
  {
+    base_url =`${url}?_sort=price&_order=desc`;
     let res =await fetch(`${url}?_sort=price&_order=desc`)
 
    let data =await res.json();
 
-   append_data(data);
+   append_data(data,base_url);
  }
  else if(sorty.value=="alphabetical")
  {
+    base_url=`${url}?_sort=detail&_order=asc`;
     let res =await fetch(`${url}?_sort=detail&_order=asc`)
 
    let data =await res.json();
 
-   append_data(data);
+   append_data(data,base_url);
  }
  else{
     get_data();
@@ -506,4 +551,103 @@ sorty.onchange=async()=>{
 
 }
 
+
+d10_15.onchange=async (event)=>{
+    if(event.target.checked)
+    {
+        let base_url=`${url}?off_like=11%`;
+        let result =await fetch (`${url}?off_like=11%`);
+
+        let fdata = await result.json();
+       
+       
+       
+        append_data(fdata,base_url);
+        if(fdata.length==0)
+        {
+         cont.innerHTML=`<h1>No Item Found</h1>`;
+        }
+    }
+    else
+    {
+        get_data();
+    }
+}
+
+d15_25.onchange=async (event)=>{
+    if(event.target.checked)
+    {
+        let base_url=`${url}?off_like=20%`;
+        let result =await fetch (`${url}?off_like=20%`);
+
+        let fdata = await result.json();
+        
+        append_data(fdata,base_url);
+        if(fdata.length==0)
+       {
+        cont.innerHTML=`<h1>No Item Found</h1>`;
+       }
+       
+    }
+    else
+    {
+        get_data();
+    }
+}
+
+
+d25.onchange=async (event)=>{
+    if(event.target.checked)
+    {
+        let base_url=`${url}?off_like=30%`;
+        let result =await fetch (`${url}?off_like=30%`);
+
+        let fdata = await result.json();
+        console.log(fdata)
+        append_data(fdata,base_url);
+        if(fdata.length==0)
+       {
+        cont.innerHTML=`<h1>No Item Found</h1>`;
+       }
+       console.log("25")
+       
+    }
+    else
+    {
+        get_data();
+    }
+}
+
+
+let pagination= async(p,base,n)=>{
+    
+    let base_url=`${base}?_page=${p}&_limit=5`;
+
+    let res=await fetch(`${base}?_page=${p}&_limit=5`);
+
+    let data =await res.json();
+
+    append_data(data,base_url);
+    page.innerHTML=null;
+    for(let i=0;i<n;i++)
+ {
+    let pbtn2=document.createElement("button");
+    pbtn2.setAttribute("class","pbtn");
+    pbtn2.innerText=i+1;
+    page.append(pbtn2);
+    let k=i+1;
+
+    pbtn2.onclick=async()=>{
+        console.log("kingkong")
+        let base_url=`${base}?_page=${k}&_limit=5`;
+
+        let res=await fetch(`${base}?_page=${k}&_limit=5`);
+    
+        let data =await res.json();
+        append_data(data,base_url);
+        pagination(k,base_url,n)
+    }
+
+}
+}
 

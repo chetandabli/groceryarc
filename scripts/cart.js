365a2b5336dc6c1c366df5b02f38ada2eb3be270
Kaushik <kaubis368@gmail.com>
2022-12-18 17:08:27 +0530

    save later added

--- scripts/cart.js
+++ scripts/cart.js
@@ -8,7 +8,7 @@ footer_new.innerHTML = footer();
 
 
 
-
+let url_later="http://localhost:3000/save_later";
 
 let url=" http://localhost:3000/cart";
 
@@ -73,9 +73,28 @@ data.forEach((el)=>{
         removeproduct(el.id);
     }
     
+    let later=document.createElement("button");
+    later.setAttribute("class","savelater");
+    later.innerText="Save as Later";
+    
+    later.onclick=async()=>{
+    
+   
+        let res =await fetch(`${main_url}/save_later`,{
+            method :"POST",
+            body: JSON.stringify(el),
+            headers:{
+                "Content-Type":"application/json"
+            }
+        })
+        localStorage.setItem("save",el.id)
+        
+        let daat = await res.json();
+        console.log(daat)
+    }
    
 
-    div.append(image,details, price,div4,remo);
+    div.append(image,details, price,div4,remo,later);
     cont.append(div);
 })
 console.log(total_amount)
@@ -83,13 +102,6 @@ document.getElementById("cart_total").innerText=`Subtotal: ₹${total_amount}`;
 
 document.getElementById("total_items").innerText=`Items: ${data.length}`;
 localStorage.setItem("order_value", total_amount);
-
-
-
-
-
-
-
 
 }
 }
@@ -104,6 +116,141 @@ get_data();
             method:"DELETE",
             
         });
-     
-        get_data(data);
+        
+        
     }
+  
+if(localStorage.getItem("save")){
+    let x=localStorage.getItem("save");
+    localStorage.removeItem("save")
+    removeproduct(x);
+}
+
+
+
+// SAVE LATER
+
+
+
+    let getdata=async()=>{
+    
+        let res=await fetch(`${url_later}`)
+        let data= await res.json();
+        console.log(data)
+        append_dat(data)
+    }
+    
+    
+    
+    
+    const append_dat=(data)=>{
+        let total_amount=0;
+        if(data.length==0){
+            document.getElementById("save_as_later").innerHTML=` <div id="init">
+            <div >
+                
+        </div>`
+    
+        }else{
+            
+    
+            let cont = document.getElementById("save_as_later");
+    cont.innerHTML=null;
+    data.forEach((el)=>{
+    
+        total_amount+=el.price;
+    
+        let div =document.createElement("div");
+        div.setAttribute("class","pro_div");
+        
+        let image = document.createElement("img");
+        image.setAttribute("class","p_image");
+        image.src=el.image_url;
+    
+        let details =document.createElement("p");
+        details.setAttribute("class","details")
+        details.innerText=el.detail;
+    
+    
+        let price =document.createElement("p");
+        price.setAttribute("class","price");
+        price.innerHTML=`<span id="mrp"><del style="color:red">${el.strike}</del></span>&nbsp;&nbsp;<spam id="bol"><span id="mrp">&#8377</span>${el.price}</spam>`;
+    
+    
+        let div4=document.createElement("div");
+        div4.setAttribute("class","div4");
+        div4.innerHTML=`<button id="minus">-</button>&nbsp;&nbsp;${el.quant}&nbsp;&nbsp;<button id="plus">+</button>`
+        
+        let remo=document.createElement("button");
+        remo.setAttribute("class","remove");
+        remo.innerText="Delete";
+        remo.onclick=()=>{
+            removeproduc(el.id);
+        }
+        
+        let later=document.createElement("button");
+        later.setAttribute("class","saveA");
+        later.innerText="Add";
+        
+        later.onclick=async()=>{
+           
+      
+       
+            let res =await fetch(`${main_url}/cart`,{
+                method :"POST",
+                body: JSON.stringify(el),
+                headers:{
+                    "Content-Type":"application/json"
+                }
+            })
+            localStorage.setItem("savereturn",el.id)
+    
+         
+           
+        }
+       
+    
+        div.append(image,details, price,div4,later,remo);
+        cont.append(div);
+    })
+    console.log(total_amount)
+    
+    
+    document.getElementById("total_item").innerText=`Items: ${data.length}`;
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+    }
+    
+    getdata();
+    
+    
+      
+    
+          let removeproduc= async(id)=>{
+            let res=await fetch(`${url_later}/${id}`,{
+                method:"DELETE",
+                
+            });
+         
+            
+        }
+    
+        if(localStorage.getItem("savereturn")){
+            let y=localStorage.getItem("savereturn");
+            localStorage.removeItem("savereturn")
+            removeproduc(y);
+        }
+        
+
+  
+
+
+    
